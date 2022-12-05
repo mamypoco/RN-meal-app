5687c258072dbad435214fb518c80d801af672a0
Mami. N <mamypoco@gmail.com>
2022-12-05 08:55:22 -0800

    displaying meals

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,24 @@
 import { FlatList } from "react-native";
 import { CATEGORIES } from "../assets/data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
+import MealsOverviewScreen from "./MealsOverviewScreen";
 
-function renderCategoryItem(itemData) {
-   return (
-      <CategoryGridTile
-         title={itemData.item.title}
-         color={itemData.item.color}
-      />
-   );
-}
+function CategoriesScreen({ navigation }) {
+   function renderCategoryItem(itemData) {
+      function pressHandler() {
+         navigation.navigate("MealsOverview", {
+            categoryId: itemData.item.id,
+         });
+      }
+      return (
+         <CategoryGridTile
+            title={itemData.item.title}
+            color={itemData.item.color}
+            onPress={pressHandler}
+         />
+      );
+   }
 
-function CategoriesScreen() {
    return (
       <FlatList
          data={CATEGORIES}
